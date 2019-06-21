333435dbc2a7c5c18d257507535691fa9505d7f9
Kurbetiev George <kgse25@gmail.com>
2019-06-22 02:22:46 +0300

    блок news + адаптив

--- js/slider.js
+++ js/slider.js
@@ -13,5 +13,24 @@ $(document).ready(function(){
 		dots: true,
 		slidesToShow: 2,
 		slidesToScroll: 2,
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			}
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		]
 	});
 })
