386470b835cf7cabb17a02b46dcbd3d9b7159378
Kurbetiev George <kgse25@gmail.com>
2019-06-25 01:36:23 +0300

    Правки по вёрстке, добавлено модальное окно и подготовка файлов валидации форм

---
+++ js/modal.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$('#button').click(function () {
+		$('#modal').addClass('modal_active');
+	});
+
+	$('#close').click(function () {
+		$('#modal').removeClass('modal_active');
+	});
+
+});
+
+
